b0d9bbeb7b78f351ba070b0ad39547eed873736d
ali000lgl-create <ali000lgl@gmail.com>
2026-06-26 21:00:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    autoHideMenuBar: true, // إخفاء القوائم العلوية المزعجة
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true
     }
   });
 
-  win.maximize(); // تشغيل البرنامج بملء الشاشة تلقائياً
-  win.loadFile('pos-system.html'); // هنا قمنا بربط ملفك الجديد
+  win.maximize();
+  win.loadFile('pos-system.html');
+
+  // أمر فحص التحديثات بمجرد فتح البرنامج
+  win.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
 app.whenReady().then(createWindow);
@@ -22,3 +28,11 @@ app.on('window-all-closed', () => {
   }
 });
 
+// إعدادات التحديث التلقائي
+autoUpdater.on('update-available', () => {
+  console.log('يوجد تحديث جديد، يتم التحميل الآن في الخلفية...');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  console.log('تم تحميل التحديث بنجاح. سيتم تثبيته عند إغلاق البرنامج.');
+});
